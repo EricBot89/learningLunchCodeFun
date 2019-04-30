db2ec9bf3f1300c2b64752d60d26715dfcc890da
zacharyAD <zacharyadcoding@gmail.com>
2019-04-30 11:37:11 -0400

    cleared code for coding interview

--- db.js
+++ db.js
@@ -1,36 +1,36 @@
 const Sequelize = require('sequelize');
 
-// when you go to start the server, be smarter than me and make sure you have postgres actually running on your machine!
-const db = new Sequelize('postgres://localhost:5432/puppies', {
-  logging: false
-});
+// initializing DB - Mess up the DB name !!!
 
-module.exports = db;
+// const db = new Sequelize('postgres://localhost:5432/puppy', {
+//   logging: false,
+// });
 
 // We'll define associations after we import them here
-const Puppy = require('./models/Puppy');
-const Park = require('./models/Park');
-const Food = require('./models/Food');
-const Location = require('./models/Location');
+
+// const Puppy = require('./models/Puppy');
+// const Food = require('./models/Food');
 
 // this will put a foreign key for parkId in the Puppy model
 // and give Puppy .setPark() and .getPark() instance methods
-Puppy.belongsTo(Park);
+
 // this will give Park the magic methods for addPuppy, etc.
 // but we already have a foreign key for parkId in the Puppy model, so it will maintain
 // the 1:m relationship
-Park.hasMany(Puppy);
 
 // Puppy to food M:M associations
 // stored in the same-named join table: 'puppiesFoods'
 // the `through` property is required in `belongsToMany` associations
 // aliased differently in each model
-Puppy.belongsToMany(Food, { as: 'favFoods', through: 'puppiesFoods' });
-Food.belongsToMany(Puppy, { as: 'puppies', through: 'puppiesFoods' });
+
+// Associateions - Start with setting up just the "through" and then bring in the "as" to show difference
+
+// Puppy.belongsToMany(Food, { as: 'favFoods', through: 'puppiesFoods' });
+// Food.belongsToMany(Puppy, { as: 'puppies', through: 'puppiesFoods' });
 
 // Puppies can have a best friend Puppy
 // in this case, you always need to alias the association
-Puppy.belongsTo(Puppy, { as: 'bestFriend' });
 
-// puts .setLocation() .getLocation() and `locationId` on the Park model
-Park.belongsTo(Location);
+// Puppy.belongsTo(Puppy, { as: 'bestFriend' });
+
+module.exports = db;
